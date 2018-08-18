61a66b6e60f1a141389de8a4eae9947d2362e893
isaiah4lyf <isaiah.ramafalo.5@gmail.com>
2018-08-18 15:29:42 +0200

    Finalized functionality

--- TRIVIA PROJECT/src/Jframe.java
+++ TRIVIA PROJECT/src/Jframe.java
@@ -6,7 +6,8 @@ import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
-
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -20,6 +21,8 @@ import javax.swing.text.AttributeSet;
 import javax.swing.text.SimpleAttributeSet;
 import javax.swing.text.StyleConstants;
 import javax.swing.text.StyleContext;
+
+
 import java.awt.Component;
 
 import java.awt.event.KeyEvent;
@@ -27,6 +30,7 @@ import java.awt.event.KeyListener;
 
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+
 
 public class Jframe extends JFrame{
 	/**
@@ -48,31 +52,67 @@ public class Jframe extends JFrame{
 	private boolean questionAnsweredCorrectly;
 	private boolean questionAnswered;
 	private boolean multiChoiceAnswered;
+	private Label timerLabel;
 	private int n;
+	private int timerCount;
+	private boolean stopTimer;
 	public Jframe()
 	{
-		String[][] questionsArray = new String[5][5];
+		String[][] questionsArray = new String[9][3];
 		questionsArray[0][0] = "What is an operating system?";
 		questionsArray[0][1] = "0";
+		questionsArray[0][2] = "10000";  //Sleep Time
 		
 		questionsArray[1][0] = "What are the main components of a modern computer?";
 		questionsArray[1][1] = "1";
+		questionsArray[1][2] = "10000";  //Sleep Time
 		
-		questionsArray[2][0] = "sd3";
+		questionsArray[2][0] = "One of modern computer's main components: \n "
+							+ "1. One or more processors \n -> Management through _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ management, and the management of processes on the machine.";
 		questionsArray[2][1] = "2";
+		questionsArray[2][2] = "11000";  //Sleep Time
 		
-		questionsArray[3][0] = "sd4";
+		questionsArray[3][0] = "One of modern computer's main components: \n "
+							+ "2. Main _ _ _ _ _ _ \n -> Allocation of _ _ _ _ _ _ that is assigned to processes, Adress spaces and Virtual _ _ _ _ _ _";
 		questionsArray[3][1] = "3";
+		questionsArray[3][2] = "10000";  //Sleep Time
 		
-		questionsArray[4][0] = "sd5";
+		questionsArray[4][0] = "One of modern computer's main components: \n "
+							+ "3. _ _ _ _ _ \n -> Files systems, allocation of blocks to files.";
 		questionsArray[4][1] = "4";
+		questionsArray[4][2] = "10000";  //Sleep Time
 		
-		AnswersArray = new String[5];
+		questionsArray[5][0] = "One of modern computer's main components: \n "
+							+ "4. Various _ _   _ _ _ _ _ _ _  \n -> The interaction between the OS and the hardware, interrupts, etc.";
+		questionsArray[5][1] = "5";
+		questionsArray[5][2] = "10000";  //Sleep Time
+		
+		questionsArray[6][0] = "What are the main components of a modern computer? \n"
+							+ "1. _ _ _ _ _ _ _ _ _ _  \n"
+							+ "2. _ _ _ _ - _ _ _ _ _ _  \n"
+							+ "3. _ _ _ _ _  \n"
+							+ "4. _ _ - _ _ _ _ _ _ _  \n";
+		questionsArray[6][1] = "6";
+		questionsArray[6][2] = "30000";  //Sleep Time
+		
+		questionsArray[7][0] = "The operating acts as _ _ _ _ _ _ _ _ - _ _ _ _ _ _ _ and _ _ _ _ _ _ _ _ - _ _ _ _ _ _ _ ";
+		questionsArray[7][1] = "7";
+		questionsArray[7][2] = "15000";  //Sleep Time
+		
+		questionsArray[8][0] = "The operating system as an extended machine: Th OS fits in _ _ _ _ _ _   _ _ _ _";
+		questionsArray[8][1] = "8";
+		questionsArray[8][2] = "10000";  //Sleep Time
+		
+		AnswersArray = new String[9];
 		AnswersArray[0] = "0,3";
 		AnswersArray[1] = "1,4";
-		AnswersArray[2] = "answer3";
-		AnswersArray[3] = "2,1";
-		AnswersArray[4] = "answer4";
+		AnswersArray[2] = "multiprocessor";
+		AnswersArray[3] = "memory";
+		AnswersArray[4] = "disks";
+		AnswersArray[5] = "io devices";
+		AnswersArray[6] = "processors main-memory disks io-devices";
+		AnswersArray[7] = "extended-machine resource-manager";
+		AnswersArray[8] = "kernel mode";
 		
 		String[] multiAnswersArray = new String[3];
 		multiAnswersArray[0] = " The operating system is the software component responsible for managing all components \n      of a computer and to provide user files with a better, simpler and cleaner model of the computer.\n"
@@ -118,11 +158,13 @@ public class Jframe extends JFrame{
 		menu_Panel.setLayout(new GridLayout(4,1));
 		
 		JPanel scoreBoardPanel = new JPanel();
-		scoreBoardPanel.setLayout(new GridLayout(2,2));
+		scoreBoardPanel.setLayout(new GridLayout(2,3));
+		timerLabel = new Label();
 		totalQuestionsAsked = new Label();
 		questionsAnswered = new Label();
 		correctAnswers = new Label();
 		percentageLabel = new Label();
+		scoreBoardPanel.add(timerLabel);
 		scoreBoardPanel.add(totalQuestionsAsked);
 		scoreBoardPanel.add(questionsAnswered);
 		scoreBoardPanel.add(correctAnswers);
@@ -152,8 +194,8 @@ public class Jframe extends JFrame{
             					questionAnswered = false;
             					multiChoiceAnswered = false;
                 				Random rand = new Random();
-                				n = rand.nextInt(4) + 0;
-                				
+                				n = rand.nextInt(9);
+                				//n = 8;
                 				String[] answer = AnswersArray[n].split(",");
                 				if(answer.length == 2)
                 				{
@@ -178,9 +220,64 @@ public class Jframe extends JFrame{
                 				
                 				Percentage = Math.round(((double)correctAnswersInt/(double)(totalQuestions-1)) * 100);
                 				percentageLabel.setText("Grade: " + String.valueOf(Percentage) + "%");
-                				Thread.sleep(10000);
-                				appendToPane(console_Like, AnswersArray[n] + "\n\n", Color.GREEN);
 
+                				timerCount = 0;
+                				stopTimer = false;
+	      				        Thread timerThread = new Thread(new Runnable() {
+	      				            @Override
+	      				            public void run() {
+	      				                try {
+	      				            		try 
+	      				            		{
+	      				            			int timeLeft = Integer.parseInt(questionsArray[n][2])/1000;
+	      				            			while(true)
+	      				            			{
+		      		      						  timerLabel.setText("Time Remaining(s):  " + String.valueOf(timeLeft-timerCount));
+		      		      						  timerCount++;
+		      		      						  Thread.sleep(1000);
+	      				            			}
+
+	      				            		} catch (Exception e) {
+	      				            			// TODO Auto-generated catch block
+	      				            			//e.printStackTrace();
+	      				            		}
+	      				                }
+	      				                catch (Exception ex)
+	      				                {
+	      				                }
+	      				            }
+	      				        });
+	      				        timerThread.start();
+                				Thread.sleep(Integer.parseInt(questionsArray[n][2]));
+                				//stopTimer = true;
+                				timerCount = 0;
+                				timerThread.interrupt();;
+                				if(answer.length == 2)
+                				{
+                					String choice = "";
+                					if(answer[1].equals("1"))
+                					{
+                						choice = "a";
+                					}
+                					else if(answer[1].equals("2"))
+                					{
+                						choice = "b";
+                					}
+                					else if(answer[1].equals("3"))
+                					{
+                						choice = "c";
+                					}
+                					else if(answer[1].equals("4"))
+                					{
+                						choice = "d";
+                					}
+                					appendToPane(console_Like, "Answer:  "+choice + "\n\n", Color.GREEN);
+                				}
+                				else
+                				{
+                					appendToPane(console_Like, "Answer:  "+ AnswersArray[n] + "\n\n", Color.GREEN);
+                				}
+        
 
             				}
             				catch(Exception e)
@@ -261,7 +358,7 @@ public class Jframe extends JFrame{
 				}
 				else
 				{
-                  	if(text.getText().replaceAll(" ", "").equals(AnswersArray[CurrentAnswer]))
+                  	if(text.getText().replaceAll(" ", "").equals(AnswersArray[CurrentAnswer].replaceAll(" ", "")))
                   	{
           				appendToPane(console_Like,"Correct! \n\n", Color.yellow);
           				
